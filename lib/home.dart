8acbcfae5c7afcf3f234efc333c67ebe3ff18af3
DevNweYi <nweyiaung610@gmail.com>
2024-01-18 22:47:40 +0630

    first commit

---
+++ lib/home.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            height: MediaQuery.of(context).size.height / 2,
+            decoration: BoxDecoration(
+                color: Colors.amber,
+                borderRadius: BorderRadius.vertical(
+                    bottom: Radius.elliptical(MediaQuery.of(context).size.width,
+                        (MediaQuery.of(context).size.height / 2) - 150))),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 20, right: 20, top: 20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                IconButton(onPressed: () {}, icon: const Icon(Icons.menu)),
+                const SizedBox(
+                  height: 30,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("Hello Ender",
+                            style: Theme.of(context)
+                                .textTheme
+                                .headlineSmall!
+                                .merge(const TextStyle(
+                                    fontWeight: FontWeight.bold))),
+                        Text("Today you have 8 Tasks",
+                            style: Theme.of(context)
+                                .textTheme
+                                .bodyMedium!
+                                .merge(const TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.black54))),
+                      ],
+                    ),
+                    Image.asset(
+                      "images/man.png",
+                      width: 50,
+                      height: 50,
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                Expanded(
+                  child: _titleDetailList(),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _titleDetailList() {
+    return ListView.builder(
+        itemCount: 10,
+        shrinkWrap: true,
+        itemBuilder: (context, index) {
+          return Card(
+            color: Colors.white,
+            margin: const EdgeInsets.only(bottom: 20),
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+            elevation: 3,
+            child: Container(
+              height: 100,
+              alignment: Alignment.center,
+              child: ListTile(
+                leading: Image.asset("images/man.png"),
+                title: Text(
+                  "Title",
+                  style: Theme.of(context).textTheme.headlineSmall!.merge(
+                      const TextStyle(
+                          fontWeight: FontWeight.bold)),
+                ),
+                subtitle: Text("$index Tasks", style: Theme.of(context).textTheme.bodyLarge!.merge(
+                      const TextStyle(
+                          fontWeight: FontWeight.bold, color: Colors.black38)),),
+              ),
+            ),
+          );
+        });
+  }
+}
---
+++ lib/task_list.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+
+class TaskList extends StatelessWidget {
+  const TaskList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          _topContainer(MediaQuery.of(context).size.width),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.only(
+                  left: 20, right: 20, top: 10, bottom: 10),
+              child: Column(
+                children: [
+                  Container(child: _taskTitle(context)),
+                  const SizedBox(height: 15,),
+                  Expanded(child: Container(child: _taskList())),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Colors.green,
+        child: const Icon(
+          Icons.add,
+          color: Colors.white,
+        ),
+      ),
+    );
+  }
+
+  Widget _taskList() {
+    return ListView.builder(
+        shrinkWrap: true,
+        itemCount: 10,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(
+              "Task $index",
+              style: Theme.of(context).textTheme.bodyLarge!.merge(
+                  const TextStyle(
+                      fontWeight: FontWeight.bold,color: Colors.black54)),
+            ),
+          );
+        });
+  }
+
+  Widget _taskTitle(BuildContext context) {
+    return Row(
+      children: [
+        Image.asset(
+          "images/man.png",
+          width: 50,
+          height: 50,
+        ),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text("8 Tasks",
+                style: Theme.of(context).textTheme.bodyLarge!.merge(
+                    const TextStyle(
+                        fontWeight: FontWeight.bold, color: Colors.grey))),
+            Text("Personal",
+                style: Theme.of(context).textTheme.displaySmall!.merge(
+                    const TextStyle(
+                        fontWeight: FontWeight.bold))),
+          ],
+        )
+      ],
+    );
+  }
+
+  Widget _topContainer(double screenWidth) {
+    return Container(
+      height: 150,
+      decoration: BoxDecoration(
+          color: Colors.amber,
+          borderRadius: BorderRadius.vertical(
+              bottom: Radius.elliptical(screenWidth, 200))),
+      child: Padding(
+        padding: const EdgeInsets.only(top: 20),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            IconButton(onPressed: () {}, icon: const Icon(Icons.arrow_back)),
+            IconButton(onPressed: () {}, icon: const Icon(Icons.more_vert)),
+          ],
+        ),
+      ),
+    );
+  }
+}
