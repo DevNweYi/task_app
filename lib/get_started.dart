099c7c6bc2edea4555a9188c78e2e4c1e06bf8f2
DevNweYi <nweyiaung610@gmail.com>
2024-01-21 21:47:58 +0630

    implementing getstarted page

--- lib/get_started.dart
+++ lib/get_started.dart
@@ -10,28 +10,45 @@ class GetStarted extends StatelessWidget {
         children: [
           Container(
             height: MediaQuery.of(context).size.height / 2,
+            width: MediaQuery.of(context).size.width,
             color: Colors.amber,
             child: Image.asset(
               "images/man.png",
-              width: 150,
-              height: 150,
+              width: 200,
+              height: 200,
             ),
           ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text("Get Organized Your Life",
-                  style: Theme.of(context)
-                      .textTheme
-                      .headlineSmall!
-                      .merge(const TextStyle(fontWeight: FontWeight.bold))),
-              Text(
-                  "Tudy is a simple and effective to-do list and task manager app which helps you manage time.",
-                  style: Theme.of(context).textTheme.bodyMedium!.merge(
-                      const TextStyle(
-                          fontWeight: FontWeight.bold, color: Colors.black38))),
-              ElevatedButton(onPressed: () {}, child: const Text("Get Started"))
-            ],
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Expanded(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text("Get Organized Your Life",
+                      textAlign: TextAlign.center,
+                          style: Theme.of(context).textTheme.headlineSmall!.merge(
+                              const TextStyle(fontWeight: FontWeight.bold))),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 10,bottom: 10,left: 50,right: 50),
+                        child: Text(
+                            "Tudy is a simple and effective to-do list and task manager app which helps you manage time.",
+                            textAlign: TextAlign.center,
+                            style: Theme.of(context).textTheme.bodyMedium!.merge(
+                                const TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.black38))),
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 30),
+                  child: ElevatedButton(onPressed: () {}, child: const Text("Get Started")),
+                )
+              ],
+            ),
           ),
         ],
       ),
